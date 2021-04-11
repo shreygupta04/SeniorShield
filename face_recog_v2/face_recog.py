dd54f82ccec89ed280432974f08e87db172061eb
atin-can <50777724+atin-can@users.noreply.github.com>
2021-04-11 02:43:12 -0400

    name update fix

--- face_recog_v2/face_recog.py
+++ face_recog_v2/face_recog.py
@@ -56,7 +56,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         if conf <= 70:
             # GPIO.output(relay_pin, 1)
             # print("Open the door, this person is there: " + name)
-            db.child("name").update({'name': name})
+            db.update({"name": name})
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
             cv2.putText(frame, name + str(conf), (x, y), font, 2, (0, 0 ,255), 2,cv2.LINE_AA)
 
@@ -69,5 +69,5 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     if key == 27 or tick==45:
         break
 
-db.child("name").update({'name': ""})
+db.update({"name": ""})
 cv2.destroyAllWindows()
