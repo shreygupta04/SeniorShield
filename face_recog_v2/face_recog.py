8bc73bf11a539184ca1f7b92ee0abd5c208cd05e
atin-can <50777724+atin-can@users.noreply.github.com>
2021-04-11 01:19:01 -0400

    update timer + bug fix

--- face_recog_v2/face_recog.py
+++ face_recog_v2/face_recog.py
@@ -22,34 +22,36 @@ recognizer.read("trainer.yml")
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
+tick = 0  
+
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	frame = frame.array
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.5, minNeighbors = 5)
-	for (x, y, w, h) in faces:
-		roiGray = gray[y:y+h, x:x+w]
+    tick+=1
+    frame = frame.array
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.5, minNeighbors = 5)
+    for (x, y, w, h) in faces:
+        roiGray = gray[y:y+h, x:x+w]
 
-		id_, conf = recognizer.predict(roiGray)
+        id_, conf = recognizer.predict(roiGray)
 
-		for name, value in dict.items():
-			if value == id_:
-				print(name)
+        for name, value in dicti.items():
+            if value == id_:
+                print(name)
 
-		if conf <= 70:
-			# GPIO.output(relay_pin, 1)
-            print("Open the door, this person is there: " + name)
-			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-			cv2.putText(frame, name + str(conf), (x, y), font, 2, (0, 0 ,255), 2,cv2.LINE_AA)
+        if conf <= 70:
+            # GPIO.output(relay_pin, 1)
+            # print("Open the door, this person is there: " + name)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.putText(frame, name + str(conf), (x, y), font, 2, (0, 0 ,255), 2,cv2.LINE_AA)
 
-		else:
-			print("no one recognized")
 
-	cv2.imshow('frame', frame)
-	key = cv2.waitKey(1)
+    cv2.imshow('frame', frame)
+    key = cv2.waitKey(1)
 
-	rawCapture.truncate(0)
+    rawCapture.truncate(0)
 
-	if key == 27:
-		break
+    if key == 27 or tick==45:
+        break
 
+print("No-one recognized")
 cv2.destroyAllWindows()
